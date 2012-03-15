7a7ad0c2578128aaf77a1b20e33c2278ce1a5166
Damián Silvani <munshkr@gmail.com>
2012-03-14 23:37:34 -0300

    initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source :rubygems
+
+gem 'sinatra'
+gem 'ruby-debug19'
+gem 'mapa76-ner', :path => '/home/munshkr/mapa76/ner'
---
+++ analyse.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'rubygems'
+require 'bundler/setup'
+require 'sinatra'
+
+require 'ner/freeling/analyzer'
+
+
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape
+end
+
+
+get '/' do
+  @files = Hash[Dir["#{settings.public_folder}/docs/*.txt"].sort.map { |path|
+    [File.basename(path), path]
+  }]
+  erb :index
+end
+
+get '/analyse' do
+  @anal = NER::FreeLing::Analyzer.new(open(params[:f]))                                                                                                                            
+  @sentences = @anal.sentences
+  erb :analyse
+end
+
+post '/analyse' do
+  'TODO'
+end
