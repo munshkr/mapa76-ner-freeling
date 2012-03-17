29e522b55d5033a41a068a2fec26aaf94830b853
Damián Silvani <munshkr@gmail.com>
2012-03-17 11:59:46 -0300

    Rename #tagged_sentences to #tagged_tokens. Raise exception on contractions

--- lib/document.rb
+++ lib/document.rb
@@ -29,7 +29,7 @@ class Document
     end
   end
 
-  # Return an array of sentences with tagged tokens.
+  # Return an array of tagged tokens.
   #
   # All tokens have a reference to the original text and its offset in the
   # string for locating it easily.
@@ -42,40 +42,39 @@ class Document
   #
   # When any of these are set, Dictionary or Tagger modules can retokenize
   # contracted words (e.g. "he's" => "he is"), changing the original text.
+  # An exception is raised if this happens.
   #
-  def tagged_sentences
+  def tagged_tokens
     Enumerator.new do |yielder|
+      # FIXME do not convert to array, use the internal iterator
       st = self.tokens.to_a
       cur_st = 0
       @analyzer[:tagged] ||= FreeLing::Analyzer.new(@text, :output_format => :tagged)
-      @analyzer[:tagged].sentences.each do |sentence|
-        toks = []
-        sentence.each do |token|
-          token.text = @text
+      @analyzer[:tagged].tokens.each do |token|
+        token.text = @text
 
-          # exact match
-          if st[cur_st].form == token.form
-            token.offset = st[cur_st].offset
+        # exact match
+        if st[cur_st].form == token.form
+          token.offset = st[cur_st].offset
+          cur_st += 1
+          yielder << token
+        # multiword
+        # e.g. John Doe ==> tokens        = ["John", "Doe"]
+        #                   tagged_tokens = ["John_Doe"]
+        elsif token.form =~ /^#{st[cur_st].form}_/
+          token.offset = st[cur_st].offset
+          token.words = {}
+          m_word = token.form.dup
+          while not m_word.empty? and m_word.start_with?(st[cur_st].form)
+            token.words[st[cur_st].offset] = st[cur_st].form
+            m_word = m_word.slice(st[cur_st].form.size + 1 .. -1).to_s
             cur_st += 1
-            toks << token
-
-          # multiword
-          # e.g. John Doe ==> tokens        = ["John", "Doe"]
-          #                   tagged_tokens = ["John_Doe"]
-          elsif token.form =~ /^#{st[cur_st].form}_/
-            token.offset = st[cur_st].offset
-            token.words = {}
-            m_word = token.form.dup
-            while not m_word.empty? and m_word.start_with?(st[cur_st].form)
-              token.words[st[cur_st].offset] = st[cur_st].form
-              m_word = m_word.slice(st[cur_st].form.size + 1 .. -1).to_s
-              cur_st += 1
-            end
-            toks << token
           end
+          yielder << token
+        else
+          raise "Simple tokens and tagged tokens do not match " \
+                "(#{st[cur_st]} != #{token}). Maybe a contraction?"
         end
-
-        yielder << toks
       end
     end
   end
