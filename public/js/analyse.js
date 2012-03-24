eba457107a71ace52a966f16aa86c7ec55293baa
Damián Silvani <munshkr@gmail.com>
2012-03-24 13:30:32 -0300

    First version of "token selection". A bit slow but usable

--- public/js/analyse.js
+++ public/js/analyse.js
@@ -1,69 +1,3 @@
-$(function() {
-
-
-  /*
-  $(".tokens").selectable({
-    filter: ".tk",
-    selected: function(event, ui) {
-      //console.log("selected: ui = " + $.map(ui, function(e) { return $(e).data("pos") }));
-    },
-  });
-
-  var start_tk = null, last_tk = null;
-
-  $(".tokens").selectable({
-    filter: ".tk",
-    selected: function(event, ui) {
-      //console.log("selected: ui = " + $.map(ui, function(e) { return $(e).data("pos") }));
-    },
-    start: function(event, ui) {
-    },
-    selecting: function(event, ui) {
-      if (!start_tk) {
-        start_tk = ui.selecting;
-      }
-      last_tk = ui.selecting;
-      if (start_tk != last_tk) {
-        var filter = 'span[data-pos="' + $(last_tk).data("pos") + '"]';
-        if ($(start_tk).data("pos") < $(last_tk).data("pos")) {
-          $(start_tk).nextUntil(filter).filter(".tk").addClass("ui-selecting");
-        } else {
-          $(start_tk).prevUntil(filter).filter(".tk").addClass("ui-selecting");
-        }
-      }
-      console.log("start_tk.pos = " + $(start_tk).data("pos") + ", last_tk.pos = " + $(last_tk).data("pos"));
-    },
-    unselecting: function(event, ui) {
-      console.log("unselected " + $(ui.unselecting).data("pos"));
-    },
-    stop: function(event, ui) {
-      start_tk = last_tk = null;
-    }
-  });
-  */
-
-
-
-
-
-  /*
-  window.Document = Backbone.Model.extend({
-
-  });
-
-  window.NamedEntitySet = Backbone.Model.extend({
-
-  });
-
-  window.NamedEntity = Backbone.Model.extend({
-
-  });
-
-  */
-
-});
-
-/*
 Array.prototype.max = function(callback) {
   return Math.max.apply(this, this.map(callback));
 };
@@ -71,4 +5,100 @@ Array.prototype.max = function(callback) {
 Array.prototype.min = function(callback) {
   return Math.min.apply(this, this.map(callback));
 };
-*/
+
+
+$(function() {
+  var start = null;
+  var selecting = false;
+  var available_targets = null;
+  var selected_targets = null;
+
+  var start_selection_on = function(target) {
+    selected_targets = [];
+    select(target);
+    start = target;
+
+    available_targets = [];
+    available_targets.push(target);
+
+    $(target).prevUntil(".ne").each(function(i, e) {
+      if ($.inArray(e, available_targets) < 0) available_targets.push(e);
+    });
+    $(target).nextUntil(".ne").each(function(i, e) {
+      if ($.inArray(e, available_targets) < 0) available_targets.push(e);
+    });
+    console.log("available: " + available_targets.length);
+
+    selecting = true;
+  };
+
+  var select = function(target) {
+    if ($.inArray(target, selected_targets) < 0) {
+      console.log("select: " + $(target).text());
+      selected_targets.push(target)
+      $(target).addClass("active");
+    }
+  };
+
+  var deselect = function(target) {
+    var pos = $.inArray(target, selected_targets)
+    if (pos >= 0) {
+      console.log("deselect: " + $(target).text());
+      selected_targets.splice(pos, 1);
+      $(target).removeClass("active");
+    }
+  };
+
+  var select_upto = function(target) {
+    $.each(available_targets, function(i, e) {
+      deselect(e);
+    });
+    select(target);
+    select(start);
+
+    var target_sel = '.tk[data-pos="' + $(target).data("pos") + '"]';
+    if ($(start).data("pos") > $(target).data("pos")) {
+      $(start).prevUntil(target_sel).filter(".tk").each(function(i, e) {
+        select(e);
+      });
+    } else if ($(start).data("pos") < $(target).data("pos")) {
+      $(start).nextUntil(target_sel).filter(".tk").each(function(i, e) {
+        select(e);
+      });
+    }
+  };
+
+
+  $(document)
+    .mouseup(function(event) {
+      console.log("up");
+
+      selecting = false;
+      $.each(selected_targets, function(i, e) {
+        $(e).removeClass("active");
+        $(e).addClass("dropped");
+        console.log("selected: " + $(e).data("pos"));
+      });
+    });
+
+  $(".tk")
+    .mousedown(function(event) {
+      if (event.button === 0) {
+        console.log("down");
+        start_selection_on(this);
+      }
+    })
+    .mouseenter(function(event) {
+      if (selecting) {
+        if ($.inArray(this, available_targets) >= 0) {
+          select_upto(this);
+        }
+      }
+    })
+    .mouseleave(function(event) {
+      if (selecting) {
+        deselect(this);
+      }
+    })
+
+});
